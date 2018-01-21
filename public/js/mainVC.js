69d2f84e8c88ad6616420d344e9ffd484b835fc1
Yuan Yao <yuanyao@Yuans-MacBook-Pro-7.local>
2018-01-21 14:58:28 -0800

    v1.001 FINISH

--- public/js/mainVC.js
+++ public/js/mainVC.js
@@ -1,3 +1,7 @@
 function didHitGetStarted() {
-
+    $('body').removeClass('offcanvas');
+    $('.js-gtco-nav-toggle').removeClass('active');
+    $('html, body').animate({
+        scrollTop: $('#gtco-started').offset().top
+    }, 1000);
 }
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
-
+var cymail = require('../modules/mails/CYMail');
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index', {title: 'Half Realty Neptune'});
+    res.render('index', {title: 'Half Realty - Neptune'});
 });
 
+router.post('/', function (req, res, next) {
+    if (req.body.service_code) {
+        if (req.body.service_code === 'register') {
+            cymail.sendEmail(req.body, function (data) {
+                res.render('thankyou', {
+                    title: 'Half-Realty Taurus'
+                });
+            });
+        }
+    } else {
+        res.send('SERVICE_CODE');
+    }
+});
 module.exports = router;
